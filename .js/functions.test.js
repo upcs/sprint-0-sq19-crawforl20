1e01a6abc005b69e28daa51cbf4f4f3a9cae3bd3
Logan Crawford <31421807+crawforl20@users.noreply.github.com>
2019-01-30 07:51:05 -0800

    Update functions.test.js

--- .js/functions.test.js
+++ .js/functions.test.js
@@ -5,5 +5,5 @@ test('adds 1 + 2 to equal 3', () => {
 
 const result2 = require('./functions');
 test('adds -2 + 8 to equal 6', () => {
-  expect(result2(-2, 8)).toBe(6);
+  expect(result2(-2, 8)).toBe(5);
 });
