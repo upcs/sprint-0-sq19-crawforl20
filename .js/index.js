09eede5f66dbf4169f7491be53fd4c22f560ca90
Logan Crawford <31421807+crawforl20@users.noreply.github.com>
2019-01-30 07:32:26 -0800

    Comlete

--- .js/index.js
+++ .js/index.js
@@ -1,1 +1,11 @@
+"use strict";
 
+function addTwo() {
+  var num1 = Number(document.getElementById('num1').value);
+  var num2 = Number(document.getElementById('num2').value);
+
+  document.getElementById('result').innerHTML = num1 + num2;
+  return false;
+}
+
+document.getElementById('sum').addEventListener('click', addTwo);
